57bbc21ee75a9417fd18b249efbb6c4662fbeae8
Michael Chapman <woppin@gmail.com>
2015-03-23 15:31:00 +1100

    Redirect output for puppet apply to bootstrap log.

--- provision/tspuppet.sh
+++ provision/tspuppet.sh
@@ -1,4 +1,4 @@
-puppet apply /etc/puppet/manifests/site.pp --detailed-exitcodes ;
+puppet apply /etc/puppet/manifests/site.pp --detailed-exitcodes >> /root/bootstrap.log;
 if (($? != 1 && $? != 4 && $? != 6)) ; then
   exit 0
 else
