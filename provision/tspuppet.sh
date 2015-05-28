b129db77ffadbb5c49f2420ae5159736c5dba071
Philip Choi <phchoi@cisco.com>
2015-05-28 14:38:15 +0800

    enable timestamp and status code tracking on tspuppet/bootstrap.log

--- provision/tspuppet.sh
+++ provision/tspuppet.sh
@@ -1,7 +1,10 @@
+date >> /root/bootstrap.log
 puppet apply /etc/puppet/manifests/site.pp --detailed-exitcodes >> /root/bootstrap.log;
 if (($? != 1 && $? != 4 && $? != 6)) ; then
+  echo "Status code $?" >> /root/bootstrap.log
   exit 0
 else
+  echo "Status code $?" >> /root/bootstrap.log
   sleep 10;
   ts /vagrant/provision/tspuppet.sh
 fi;
